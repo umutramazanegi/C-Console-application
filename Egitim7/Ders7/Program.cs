627ee1d8b3a37239e988714b6709e574527b61b6
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2023-03-31 00:08:35 +0300

    Kelime Ters Çevirici

---
+++ Egitim7/Ders7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kelime Ters Çevirici");
+
+            Console.Write("Cümleyi giriniz: ");
+            string cumle = Console.ReadLine();
+
+            string[] kelimeler = cumle.Split(' ');
+
+            Console.WriteLine("\nKelime harfleri ters çevrilmiş hali:");
+
+            foreach (string kelime in kelimeler)
+            {
+                char[] karakterDizisi = kelime.ToCharArray();
+                Array.Reverse(karakterDizisi);
+                Console.Write(new string(karakterDizisi) + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
