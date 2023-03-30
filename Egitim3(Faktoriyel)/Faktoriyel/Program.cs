f4c60353901438b8333c6ad01cba2e887b61c23e
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2023-03-30 23:34:23 +0300

    Faktoriyel Hesaplama

---
+++ Egitim3(Faktoriyel)/Faktoriyel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    internal class Program
+    {
+        //faktoriyel
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktöriyel Hesaplayıcı");
+
+            Console.Write("Faktöriyeli hesaplanacak sayıyı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int faktoriyel = 1;
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel *= i;
+            }
+
+            Console.WriteLine("\n" + sayi + "! = " + faktoriyel);
+
+            Console.ReadKey();
+        }
+    }
+}
