41db307ef0fbb64ddcbc09286926f223ea9229c2
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2023-03-30 22:47:57 +0300

    Hesap Makinesi Basit

---
+++ Egitim2(HesapMakinesi)/Egitim2(HesapMakinesi)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim2_HesapMakinesi_
+{
+    internal class Program
+    {
+        /*Basit bir hesap makinesi uygulaması */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hesap Makinesi");
+
+            Console.Write("Birinci sayıyı giriniz: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı giriniz: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nToplama işlemi: " + (sayi1 + sayi2));
+            Console.WriteLine("Çıkarma işlemi: " + (sayi1 - sayi2));
+            Console.WriteLine("Çarpma işlemi: " + (sayi1 * sayi2));
+            Console.WriteLine("Bölme işlemi: " + (sayi1 / sayi2));
+
+            Console.ReadKey();
+        }
+    }
+}
