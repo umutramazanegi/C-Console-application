cba32492ec18cad94aac5acc86414168ff42c896
Umut Ramazan Eği <78850577+umutramazanegi@users.noreply.github.com>
2023-03-30 23:41:11 +0300

    Metni Ters Cevirme

---
+++ Egitim4/Egitim4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egitim4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metin Ters Çevirici");
+
+            Console.Write("Metni giriniz: ");
+            string metin = Console.ReadLine();
+
+            char[] karakterDizisi = metin.ToCharArray();
+            Array.Reverse(karakterDizisi);
+
+            Console.WriteLine("\nTers çevrilmiş metin: " + new string(karakterDizisi));
+
+            Console.ReadKey();
+        }
+    }
+}
